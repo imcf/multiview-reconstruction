b82a6f70237664123bb91c04eadc49dccc64a147
lguerard <laurent.guerard@unibas.ch>
2021-11-08 09:34:07 +0100

    Clean up before PR

--- src/main/java/net/preibisch/mvrecon/fiji/plugin/resave/Resave_TIFF.java
+++ src/main/java/net/preibisch/mvrecon/fiji/plugin/resave/Resave_TIFF.java
@@ -38,7 +38,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.lang.reflect.Field;
 
 import bdv.export.ProgressWriter;
 import fiji.util.gui.GenericDialogPlus;
